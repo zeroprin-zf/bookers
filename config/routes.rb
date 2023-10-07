bc6156f6aa95c183bddb64ca2110330803eb19a5
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-07 05:59:27 +0000

    viewファイルの作成

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get '/new' => 'books#new'
+  get '/index' => 'books#index'
+  get '/show' => 'books#show'
+  get '/edit' => 'books#edit'
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
