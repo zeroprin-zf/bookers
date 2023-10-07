2d949dfcaafc895c5df7d391d393df9092501e31
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-07 10:48:06 +0000

    indexにshowへのリンクを作ったところ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(list_params)
     book.save
@@ -10,14 +10,16 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
   def list_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'books/new' => 'books#new'
-  get '/index' => 'books#index'
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get '/show' => 'books#show'
   get '/edit' => 'books#edit'
   get '/top' => 'homes#top'
+  get 'books/:id' => 'books#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
