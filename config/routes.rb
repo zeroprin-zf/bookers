8a0f87c59c657ae6d7fec1266e338a4aeb5d24f0
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-08 02:08:42 +0000

    編集機能まで

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'books/new' => 'books#new'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get '/edit' => 'books#edit'
+  get 'books/:id/edit' => 'books#edit', as: 'book'
   get '/top' => 'homes#top'
   get 'books/:id' => 'books#show'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
