04696e596ad5ccae5549e6c4448a90664256a3a4
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-08 05:20:15 +0000

    start画面遷移したいところまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,17 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(list_params)
+    book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
   end
 
   def index
     @books = Book.all
+   # @book = Book.find(params[:id])
+    #book = Book.find(params[:id])
+    #book.update(book_params)
+    #redirect_to book_path(book.id) 
   end
 
   def show
@@ -20,15 +24,21 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   private
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new' => 'books#new'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id/edit' => 'books#edit', as: 'book'
   get '/top' => 'homes#top'
-  get 'books/:id' => 'books#show'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
