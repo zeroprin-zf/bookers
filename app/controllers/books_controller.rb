9d1d9097ba6cda0ea1b794dc38298a870e2e3eaf
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-09 09:50:32 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,15 @@ class BooksController < ApplicationController
   end
 
   def create
+
     @book = Book.new(book_params)
-    @book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -21,17 +26,20 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully update."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
