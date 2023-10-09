57d9a2cb98f9271288afe78513a187a5ae8d60f7
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-09 12:15:59 +0000

    editのエラーメッセージができてるかわからない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,9 +31,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully update."
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
